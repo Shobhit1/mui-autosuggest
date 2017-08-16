e1b1c3854bea371b886e0231dea197145ca9d79e
Shobhit Agarwal <shobhit657@gmail.com>
2017-08-15 22:14:51 -0500

    fix some test cases

--- src/AsyncAutoSelect.spec.js
+++ src/AsyncAutoSelect.spec.js
@@ -1,4 +1,3 @@
-import expect from 'expect'
 import React from 'react'
 import { shallow } from 'enzyme'
 import AsyncAutoSelect from './AsyncAutoSelect'
@@ -16,61 +15,45 @@ describe('AsyncAutoSelect', () => {
   }
 
   it('initializes with empty options', () => {
-    const makeAutoSelectAsync = shallow(<AsyncAutoSelect {...requiredProps} fetchOptions={(setOptions) => setOptions([1, 2, 3])} />)
+    const makeAutoSelectAsync = shallow(<AsyncAutoSelect {...requiredProps} fetchOptions={(setOptions) => setOptions([1, 2, 3])} />)    
+    expect(makeAutoSelectAsync).toMatchSnapshot()
     expect(makeAutoSelectAsync.state('options')).toEqual([])
   })
 
   it('gets options asynchronously via fetchOptions on first focus', (done) => {
     const newOptions = [3, 4, 5]
     let makeAutoSelectAsync
-    const fetchOptions = (setOptions) => setTimeout(() => {
-      setOptions(newOptions, () => {
-        expect(makeAutoSelectAsync.state('options')).toBe(newOptions)
-        done()
-      })
-    }, 0)
-    makeAutoSelectAsync = mount(
-      <AsyncAutoSelect
-        {...requiredProps}
-        fetchOptions={fetchOptions}
-      />
-    )
+    const fetchOptions = (setOptions) =>
+      setTimeout(() => {
+        setOptions(newOptions, () => {
+          expect(makeAutoSelectAsync.state('options')).toBe(newOptions)
+          done()
+        })
+      }, 0)
+    makeAutoSelectAsync = mount(<AsyncAutoSelect {...requiredProps} fetchOptions={fetchOptions} />)
 
     makeAutoSelectAsync.find('input[type="text"]').simulate('focus')
   })
 
   it('does not fire fetchOptions on second focus if fetchOptions has not changed', () => {
-    const fetchOptions = expect.createSpy((setOptions) =>
-      setOptions([1, 2, 3])
-    ).andCallThrough()
-    const makeAutoSelectAsync = mount(
-      <AsyncAutoSelect
-        {...requiredProps}
-        fetchOptions={fetchOptions}
-      />
-    )
+    const fetchOptions = jest.fn((setOptions) => setOptions([1, 2, 3]))
+    const makeAutoSelectAsync = mount(<AsyncAutoSelect {...requiredProps} fetchOptions={fetchOptions} />)
 
     makeAutoSelectAsync.find('input[type="text"]').simulate('focus')
     makeAutoSelectAsync.find('input[type="text"]').simulate('focus')
 
-    expect(fetchOptions.calls.length).toEqual(1)
+    expect(fetchOptions).toHaveBeenCalled()
   })
 
   it('refires fetchOptions on second focus if fetchOptions has changed after first focus', () => {
-    const fetchOptions1 = expect.createSpy((setOptions) => setOptions([1, 2, 3]))
-    const fetchOptions2 = expect.createSpy((setOptions) => setOptions([1, 2, 3]))
-    const makeAutoSelectAsync = mount(
-      <AsyncAutoSelect
-        {...requiredProps}
-        fetchOptions={fetchOptions1}
-      />
-    )
-
+    const fetchOptions1 = jest.fn((setOptions) => setOptions([1, 2, 3]))
+    const fetchOptions2 = jest.fn((setOptions) => setOptions([1, 2, 3]))
+    const makeAutoSelectAsync = mount(<AsyncAutoSelect {...requiredProps} fetchOptions={fetchOptions1} />)
 
     makeAutoSelectAsync.find('input[type="text"]').simulate('focus')
     makeAutoSelectAsync.setProps({ fetchOptions: fetchOptions2 })
     makeAutoSelectAsync.find('input[type="text"]').simulate('focus')
 
-    expect([fetchOptions1, fetchOptions2].map((spy) => spy.calls.length)).toEqual([1, 1])
+    expect([fetchOptions1, fetchOptions2].map((spy) => spy.mock.calls.length)).toMatchSnapshot()
   })
 })
